438661b2a8d115cf43aeb3363b60c1ad329edf5c
nithi23 <107340249+nithi23@users.noreply.github.com>
2022-07-28 19:08:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "= 1.2.6" // added latest version
+    required_version = "~> 1.2.6" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
